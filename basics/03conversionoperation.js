964cca30c19b9f1a2f67560158f0afa7556f9c74
QUBITABHAY <codingcontestabhay@gmail.com>
2024-11-17 00:07:14 +0530

    conversion of datatype

---
+++ basics/03conversionoperation.js
@@ -0,0 +1,28 @@
+let score = undefined
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "199" => 33 mean in string to number
+// "199abc" => NaN is output which is not a number  but type is number
+// true => 1: false => 0
+// 
+
+let isLoggedIn = "jd"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false 
+// "" => false
+// "ga" => true
+
+let someNumber = 58
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
+
