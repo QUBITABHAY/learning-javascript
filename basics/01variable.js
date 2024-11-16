0c9ced108a2381f3130b941742e1bb76f25a41bd
QUBITABHAY <codingcontestabhay@gmail.com>
2024-11-16 23:51:12 +0530

    changes

---
+++ basics/01variable.js
@@ -0,0 +1,14 @@
+const accountId = 2772387
+let accountEmail = "abhay@gmail.com"
+var accountPassward = "567"
+accountCity = "Jaunpur"
+
+// cont cant't be changed
+// let can be changed
+/*
+Prefer not to use
+var because of issue in bloock scope functional scope
+*/
+
+console.log(accountEmail)
+console.table([accountId, accountEmail, accountPassward, accountCity])
---
+++ basics/02datatype.js
@@ -0,0 +1,23 @@
+"use stict"; // treat all JS codes newer version
+
+
+// alert("hello")   this is usable in browser
+
+
+let name="Abhay"
+let age=19
+let isLoggedIn=false
+let state;
+
+
+// number  =>2 to power 53
+// bigint
+// string
+// blooean
+// null =>standalone value
+// undefined =>
+// symbol =>unique
+// object
+
+console.log(typeof null)  // it gives object
+console.log(typeof undefined)  // it gives undefined
