bbc11879b155ccdb19fbeedf824411e8c8c5bde3
QUBITABHAY <codingcontestabhay@gmail.com>
2024-11-18 23:59:41 +0530

    Added more content

--- basics/03conversionoperation.js
+++ basics/03conversionoperation.js
@@ -1,28 +1,65 @@
-let score = undefined
-console.log(typeof score);
-console.log(typeof(score));
+let score = "abhay"
 
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
-// "199" => 33 mean in string to number
-// "199abc" => NaN is output which is not a number  but type is number
-// true => 1: false => 0
-// 
 
-let isLoggedIn = "jd"
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "abhay"
+
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn);
 
-
-// 1 => true; 0 => false 
+// 1 => true; 0 => false
 // "" => false
-// "ga" => true
+// "hitesh" => true
 
-let someNumber = 58
-let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+let someNumber = 33
 
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " abhay"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
