7de5b5f1771c76dcf7e5089eec28ce1939e2fd9d
QUBITABHAY <codingcontestabhay@gmail.com>
2024-11-22 23:45:50 +0530

    scope

---
+++ 03basics/02_scopes.js
@@ -0,0 +1,15 @@
+// var c = 300
+
+if (true) {
+    let a = 10
+    const b = 20
+    // c = 30
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+console.log(c);
+
