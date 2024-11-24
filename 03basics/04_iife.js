dc4c23840896cd174f2102f10ffa1cf6826f0d01
QUBITABHAY <codingcontestabhay@gmail.com>
2024-11-24 21:10:22 +0530

    IIFE

---
+++ 03basics/04_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function one() {
+    // namd IIFE
+    console.log(`hello`);
+    
+})();
+
+( () => {
+    // 
+    console.log(`ok`);
+    
+} ) ()
