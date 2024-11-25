9596e867d539a6c45d8a968c45d5a2fc8141dacb
QUBITABHAY <codingcontestabhay@gmail.com>
2024-11-25 23:25:17 +0530

    switch done

--- 04control_flow/switch.js
+++ 04control_flow/switch.js
@@ -7,3 +7,22 @@
 //         break;
 // }
 
+const mon = 10
+
+switch (mon) {
+    case 1:
+        console.log("Something");
+        break;
+    case 2:
+        console.log("ok");
+        break;
+    case 3:
+        console.log("done");
+        break;
+    case 3:
+        console.log("yes");
+        break;
+    default:
+        console.log("default");
+        break;
+}
