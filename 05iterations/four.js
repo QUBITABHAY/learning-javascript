9894f20feb4239be4a4d4f0f4f3941ce2357cc49
QUBITABHAY <codingcontestabhay@gmail.com>
2024-11-27 23:47:48 +0530

    forin

---
+++ 05iterations/four.js
@@ -0,0 +1,25 @@
+const myObject = {
+    k: "but",
+    see: "let's see",
+}
+
+for (const key in myObject) {
+    // console.log(key);
+    
+}
+
+const a = ["wfgvhb", "whgveh", "wgfukyqgf"]
+for (const key in a) {
+    // console.log(a[key]);
+    
+}
+
+const map = new Map()
+map.set("h", "j")
+map.set("ho", "j")
+map.set("dl", "y2")
+
+// for (const key in map) {
+//     console.log(key);
+            //  no output for map
+// }
