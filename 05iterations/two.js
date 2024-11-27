431ec8803d07675e64ea2cc241a66e6914057ed1
QUBITABHAY <codingcontestabhay@gmail.com>
2024-11-27 23:27:46 +0530

    while loop

---
+++ 05iterations/two.js
@@ -0,0 +1,22 @@
+// let arr = 0;
+
+// while (arr < 10) {
+//     // console.log(arr);
+//     arr += 2
+    
+// }
+
+// let myArray = [1, 2, 3, 4, 5, 6, 7]
+
+// let a = 0
+// while (a < myArray.length) {
+//     // console.log(a);
+//     a += 1
+// }
+
+let score = 11
+
+do {
+    console.log(score);
+    score++
+} while (score < 10);
