7586b677a99dd70248f5ac515e3cbb189645f914
Abhay Pratap Yadav <codingcontestabhay@gmail.com>
2024-12-17 18:58:46 +0530

    Update 10classes_and_oop

---
+++ 10classes_and_oop/call.js
@@ -0,0 +1,15 @@
+function SetUsername(username){
+    //complex DB calls
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username, email, password){
+    SetUsername.call(this, username)
+   
+    this.email = email
+    this.password = password
+}
+
+const thing = new createUser("thing", "thing@fb.com", "123")
+console.log(thing);
