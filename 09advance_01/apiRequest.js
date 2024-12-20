50e045de5b85ea50d572fbc667aa3ab2e2195e9b
QUBITABHAY <codingcontestabhay@gmail.com>
2024-12-20 22:29:26 +0530

    testing new feature. This is unfinished

--- 09advance_01/apiRequest.js
+++ 09advance_01/apiRequest.js
@@ -1,7 +1,7 @@
 const url = "https://api.github.com/users/QUBITABHAY"
 const hxr = new XMLHttpRequest();
 hxr.open("GET", url)
-hxr.onreadystatechange = function () {
+hxr.onreadystatechange = function (k) {
     console.log(hxr.readyState);
     if (hxr.readyState === 4) {
         const data = JSON.parse(this.responseText)
@@ -12,3 +12,15 @@ hxr.send()
 
 
 const card = document.getElementById("card")
+
+window.addEventListener("click", function (e) {
+    console.log(e)
+    console.log(e.target)
+    if (e.target.id === "card") {
+        insert.innerHTML = `
+            <div>
+                <img src="${k.data.avatar_url}" alt="not avilable">
+            </div>
+        `
+        }
+    })
