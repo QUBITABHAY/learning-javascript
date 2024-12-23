0489666f33f214136a5bc2ba30ab08c142cf20f2
QUBITABHAY <codingcontestabhay@gmail.com>
2024-12-23 19:27:09 +0530

    Update apiRequest.js

--- 09advance_01/apiRequest.js
+++ 09advance_01/apiRequest.js
@@ -1,26 +1,31 @@
 const url = "https://api.github.com/users/QUBITABHAY"
 const hxr = new XMLHttpRequest();
 hxr.open("GET", url)
-hxr.onreadystatechange = function (k) {
+hxr.onreadystatechange = function () {
     console.log(hxr.readyState);
     if (hxr.readyState === 4) {
         const data = JSON.parse(this.responseText)
         console.log(data.followers);
     }
 }
-hxr.send()
-
 
 const card = document.getElementById("card")
+const main = document.querySelector("body")
 
-window.addEventListener("click", function (e) {
-    console.log(e)
-    console.log(e.target)
-    if (e.target.id === "card") {
-        insert.innerHTML = `
-            <div>
-                <img src="${k.data.avatar_url}" alt="not avilable">
-            </div>
-        `
+main.forEach(function (cards) {
+    cards.addEventListener("click", function (e) {
+        console.log(e);
+        console.log(e.target);
+        if (hxr.readyState === 4 && e.target.id === "card") {
+            const data = JSON.parse(this.responseText)
+                insert.innerHTML = `
+                    <div>
+                        <img src="${data.avatar_url}" alt = "problem in fetching data">
+                        <h1>This is the profile pic of Github profile</h1>
+                    </div>
+                `
         }
     })
+})
+
+hxr.send()
